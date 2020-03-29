08c5c8db0b7070583353decbf45a62142d0ec70f
junyeok <junyeok@gmail.com>
2020-03-29 09:25:52 +0900

    Improvement solution of CheckInclusion.

--- CheckInclusion.cpp
+++ CheckInclusion.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <array>
 
 class CheckInclusion
 {
 public:
-  bool checkInclusion(std::string s1, std::string s2)
-  {
-    int start = s2.find(s1[0],0);
+  bool checkInclusion(std::string s1, std::string s2) {
+
     size_t s1_size = s1.size();
     size_t s2_size = s2.size();
 
@@ -15,22 +15,22 @@ public:
       return false;
     }
     
-    if(start == std::string::npos) {
-      return false;
-    } else {
-      start = std::max(0,start - (int)s1_size + 1);
+    std::array<int,26> s1_map = {0};
+    std::array<int,26> s2_map = {0};
+
+    for(size_t i = 0; i < s1_size; ++i) {
+      s1_map[s1[i] - 'a']++;
+      s2_map[s2[i] - 'a']++;
     }
 
-    std::sort(s1.begin(),s1.end());
-
-    for(size_t i = 0; i < s2_size - s1_size + 1; ++i) {
-      std::string sub_str2 = s2.substr(i,s1_size);
-      std::sort(sub_str2.begin(),sub_str2.end());
-      if(s1 == sub_str2) {
+    for(size_t i = 0; i < s2_size - s1_size; ++i) {
+      if(s1_map == s2_map) {
         return true;
       }
+      s2_map[s2[i] - 'a']--;
+      s2_map[s2[s1_size + i] - 'a']++;
     }
 
-    return false;
+    return s1_map == s2_map;
   }
 };
