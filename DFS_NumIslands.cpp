25923a36d98736795575068f9749826e14b48b79
junyeok <junyeok@gmail.com>
2020-04-07 21:12:34 +0900

    Solve NumIsland.

--- DFS_NumIslands.cpp
+++ DFS_NumIslands.cpp
@@ -15,9 +15,9 @@ class NumIslands {
 
 
   int numIslands(std::vector<std::vector<char>>& grid) {
-    
+    if(grid.size() == 0) return 0; 
     auto KeyCompare = [&grid](const Key& lh, const Key& rh) {
-      return grid[lh.row][lh.col] > grid[rh.row][rh.col];
+      return grid[lh.row][lh.col] < grid[rh.row][rh.col];
     };
 
     std::priority_queue<Key,std::vector<Key>,std::function<bool(Key, Key)>> pq(KeyCompare);
@@ -34,19 +34,25 @@ class NumIslands {
       
       visited[top.row][top.col] = true;
       
+      bool to_continue = false;
       if(grid[top.row][top.col] == '1') {
         if (!top.continuing) {
           ret += 1;
         }
-        if((top.row + 1 < grid.size() - 1) && (top.col + 1 < grid[0].size()) ) {
-          pq.push( Key{top.row + 1, top.col, true});
-          pq.push( Key{top.row, top.col + 1, true});
-        }
-      } else {
-        if((top.row + 1 < grid.size() - 1) && (top.col + 1 < grid[0].size()) ) {
-          pq.push( Key{top.row + 1, top.col, false});
-          pq.push( Key{top.row, top.col + 1, false});
-        }
+        to_continue = true;
+      }
+
+      if(top.row + 1 < grid.size()) {
+        pq.push( Key{top.row + 1, top.col, to_continue});
+      }
+      if(top.row - 1 >= 0) {
+        pq.push( Key{top.row - 1, top.col, to_continue});
+      }
+      if(top.col + 1 < grid[0].size()) {
+        pq.push( Key{top.row, top.col + 1, to_continue});
+      }
+      if(top.col - 1 >= 0) {
+        pq.push( Key{top.row, top.col - 1, to_continue});
       }
     }
 
