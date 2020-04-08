43a24db12e64bd7470598eb5fe0ae2317db76fc0
junhyeok_park <junhyeok_park@tmax.co.kr>
2020-04-08 10:06:03 +0900

    Fix some error.

--- DFS_NumIslands.cpp
+++ DFS_NumIslands.cpp
@@ -15,44 +15,56 @@ class NumIslands {
 
 
   int numIslands(std::vector<std::vector<char>>& grid) {
-    if(grid.size() == 0) return 0; 
+    if(grid.size() == 0) return 0;
+
     auto KeyCompare = [&grid](const Key& lh, const Key& rh) {
       return grid[lh.row][lh.col] < grid[rh.row][rh.col];
     };
-
-    std::priority_queue<Key,std::vector<Key>,std::function<bool(Key, Key)>> pq(KeyCompare);
+    std::priority_queue< Key, std::vector<Key>, std::function<bool(Key, Key)> > pq(KeyCompare);
     std::vector<std::vector<bool>> visited(grid.size(),std::vector<bool>(grid[0].size(),false));
 
-    pq.push( Key{0,0,false} );
-
     int ret = 0;
+
+    pq.push( Key{0,0,false} );    
     while (!pq.empty()) {
       const auto top = pq.top();
       pq.pop();
-      
-      if(visited[top.row][top.col]) continue;
-      
+
       visited[top.row][top.col] = true;
       
-      bool to_continue = false;
       if(grid[top.row][top.col] == '1') {
         if (!top.continuing) {
           ret += 1;
         }
-        to_continue = true;
-      }
-
-      if(top.row + 1 < grid.size()) {
-        pq.push( Key{top.row + 1, top.col, to_continue});
-      }
-      if(top.row - 1 >= 0) {
-        pq.push( Key{top.row - 1, top.col, to_continue});
-      }
-      if(top.col + 1 < grid[0].size()) {
-        pq.push( Key{top.row, top.col + 1, to_continue});
-      }
-      if(top.col - 1 >= 0) {
-        pq.push( Key{top.row, top.col - 1, to_continue});
+        if(top.row + 1 < grid.size() && !visited[top.row + 1][top.col]) {
+          pq.push( Key{top.row + 1, top.col, true});
+        }
+        if(top.row - 1 >= 0 && !visited[top.row - 1][top.col]) {
+          pq.push( Key{top.row - 1, top.col, true});
+        }
+        if(top.col + 1 < grid[0].size() && !visited[top.row][top.col + 1]) {
+          pq.push( Key{top.row, top.col + 1, true});
+        }
+        if(top.col - 1 >= 0 && !visited[top.row][top.col - 1]) {
+          pq.push( Key{top.row, top.col - 1, true});
+        }
+      } else {
+        if(top.row + 1 < grid.size() && !visited[top.row + 1][top.col]) {
+          pq.push( Key{top.row + 1, top.col, false});
+          if(grid[top.row + 1][top.col] == '1') continue;
+        }
+        if(top.row - 1 >= 0 && !visited[top.row - 1][top.col]) {
+          pq.push( Key{top.row - 1, top.col, false});
+          if(grid[top.row - 1][top.col] == '1') continue;
+        }
+        if(top.col + 1 < grid[0].size() && !visited[top.row][top.col + 1]) {
+          pq.push( Key{top.row, top.col + 1, false});
+          if(grid[top.row][top.col + 1] == '1') continue;
+        }
+        if(top.col - 1 >= 0 && !visited[top.row][top.col - 1]) {
+          pq.push( Key{top.row, top.col - 1, false});
+          if(grid[top.row][top.col - 1] == '1') continue;
+        }
       }
     }
 
