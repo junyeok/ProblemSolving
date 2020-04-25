6f91390abb712a1ce3631500bb3c85029d2f22e0
junyeok <junyeok@gmail.com>
2020-04-25 14:23:42 +0900

    Add some comments in ShortestSubarray.cpp.

--- QUEUE_ShortestSubarray.cpp
+++ QUEUE_ShortestSubarray.cpp
@@ -11,7 +11,7 @@ public:
 		
 		std::vector<std::vector<int>> save(len,std::vector<int>(len,0));
 
-		int shortest_len = INT_MAX;
+		int shortest_len = len + 1;
 
 		for(int i = 0; i < len; ++i) {
 			save[i][i] = A[i];
@@ -27,13 +27,22 @@ public:
 			}
 		}
 
-		return shortest_len == INT_MAX ? -1 : shortest_len;
+		return shortest_len == len + 1 ? -1 : shortest_len;
 	}
 
 	/**Dequeue를 활용한 풀이
+	 * 
 	 * B[i+1] = A[0] + A[1] + ... A[i]인 subsum B[i]를 정의하자.
 	 * 두 index x1, x2에 대해 B(x2) - B(x1) = A(x1) + A(x1+2) + ... + A(x2-1)을 만족하므로
 	 * B(x2) - B(x1) >= K이면 조건을 만족하는 subarray legnth는 x2-x1이다.
+	 * 
+	 * 상세 풀이는 다음과 같다.
+	 * 	- A index에 대한 for문을 돌면서 deque에 index를 저장한다.
+	 * 	- 만약 현재 index i에서 B(i) - B(deque.front) >= K를 만족할 경우 shortest len을 갱신하고 front pop을 수행한다.
+	 * 		-- pop을 해도 되는 이유는, i 뒤의 index가 위 조건을 만족한다고 하더라도 i-front이 값이 가장 shortest length이기 때문이다.
+	 * 	- 추가적으로, B(deque.back)이 B(i)보다 크거나 같은 경우 pruning이 가능하다.
+	 * 		-- 뒤에서 위 조건을 만족하는 index y가 존재할 때, B(y)-B(deque.back)을 만족한다면 B(y)-B(i)도 만족하며,
+	 * 			 이 경우 shortest length는 y-i이기 때문이다.
 	 */
 	int shortestSubarray2(std::vector<int>& A, int K) {
 		int len = A.size();
@@ -46,24 +55,19 @@ public:
 		}
 
 		std::deque<int> idx;
-		int shortest_len = INT_MAX;
+		int shortest_len = len + 1;
 
 		for(int i = 0; i < len + 1; ++i) {
-			/* B(x2) - B(x1) = A(x1) + A(x1+2) + ... + A(x2-1) */
 			while(!idx.empty() && B[i] >= B[idx.front()] + K) {
 				shortest_len = std::min(shortest_len, i - idx.front());
 				idx.pop_front();
 			}
-			/* i > idx.back() 일 때 B[idx.back()] 보다 B[i]가 더 작은 경우,
-				 i 뒤에 y-i >= K을 만족하는 어떤 y가 존재한다고 하자.
-				 이 때 y-idx.back() >= K를 만족할지라도, shortest length는 y-i이다.
-				 즉 어떠한 경우에도 idx.back()이 시작 x로 작용할 여지는 없다.  */
 			while(!idx.empty() && B[i] <= B[idx.back()]) {
 				idx.pop_back();
 			}
 			idx.push_back(i);
 		}
 
-		return shortest_len == INT_MAX ? -1 : shortest_len;		
+		return shortest_len == len + 1 ? -1 : shortest_len;		
 	}
 };
